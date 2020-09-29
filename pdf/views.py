5b64d845ca458358b12845f349a2b096d917f0cc
CKdave <chirag.django@gmail.com>
2020-09-30 04:50:35 +0530

    first commit

---
+++ pdf/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io
+#
+# path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+# config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+# pdfkit.from_url('pdf/resume.html', "resume.pdf", configuration=config)
+
+
+def accept(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        summary = request.POST.get('summary', '')
+        degree = request.POST.get('degree', '')
+        school = request.POST.get('school', '')
+        university = request.POST.get('university', '')
+        previous_work = request.POST.get('previous_work', '')
+        skills = request.POST.get('skills', '')
+
+        profile = Profile(name=name,email=email,phone=phone,
+                          summary=summary,degree=degree,school=school,
+                          university=university,previous_work=previous_work,skills=skills)
+        profile.save()
+
+    return render(request,'pdf/accept.html')
+
+def resume(request,pk):
+    user_profile = Profile.objects.get(pk=pk)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options = {
+        'page-size': 'Letter',
+        'encoding': 'UTF-8'
+    }
+    pdf = pdfkit.from_string(html,False,options=options)
+    response = HttpResponse(pdf,content_type='application/pdf')
+    filename = 'resume.pdf'
+    response['Content-Disposition'] = 'attachment ; filename= "' + filename + '"'
+
+    return response
+
+def list(request):
+    profiles = Profile.objects.all()
+    return render(request,'pdf/list.html',{'profiles':profiles})
